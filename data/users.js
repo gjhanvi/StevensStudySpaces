067f5ac3a704f84fed365412147f1a267053914c
Susan McAloon <smcaloon@stevens.edu>
2022-11-30 16:02:16 -0500

    added package.json

--- data/users.js
+++ data/users.js
@@ -1,7 +1,21 @@
-const createUser = async (
-    username, password
-  ) => { };
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const mongoCollections = require('../config/mongoCollections');
+const users = mongoCollections.users;
+
+
+const createUser = async (username, password) => {
+    username = validation.checkUsername(username);
+    password = validation.checkPassword(password);
+
+
+  };
   
-  const checkUser = async (username, password) => { };
+  const checkUser = async (username, password) => {
+    username = validation.checkUsername(username);
+    password = validation.checkPassword(password);
+
+
+   };
   
   module.exports = {};
