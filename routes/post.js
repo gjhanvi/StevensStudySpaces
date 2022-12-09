60d549e4d3a0248de4bb7bafa9d7d21510f3b8a5
steventruong1 <struong1@stevens.edu>
2022-12-09 18:56:14 -0500

    added placeholders for code

--- routes/post.js
+++ routes/post.js
@@ -2,15 +2,51 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const helpFunctions = require("../helpers.js");
-const postData = require("../data/posts.js");
+const postdata = require("../data/users.js");
 
 router
-  .route('/posts')
+  .route('/')
   .get(async (req, res) => {
     //code here for GET
     if(req.session.user)
     {
-      //res.redirect('/protected');
+      //const postList = await postdata.getAllPosts();
+      //res.render('userLogin', {title: "Login"}); Need tp render a page that shows all the posts.
+    }
+    else
+    {
+      res.render('userLogin', {title: "Login"});
+    }
+  })
+router
+  .route("/new")
+  .get(async (req, res) => 
+  {
+    if(req.session.user)
+    {
+      //res.render('userLogin', {title: "Login"}); Need to render a page that shows the post form
+    }
+    else
+    {
+      res.render('userLogin', {title: "Login"});
+    }
+  })
+  .post(async (req, res) => {
+    if(req.session.user)
+    {
+      //const postList = await postdata.addPost();
+      //res.render('userLogin', {title: "Login"}); Needs to add a new post
+    }
+    else
+    {
+      res.render('userLogin', {title: "Login"});
+    }
+  })
+  .delete(async (req, res) => {
+    if(req.session.user)
+    {
+      //const postList = await postdata.deletePost(CHANGEME);
+      //res.render('userLogin', {title: "Login"}); Needs to add a new post
     }
     else
     {
