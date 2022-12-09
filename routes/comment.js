d420a16402b50bfd509f903e227ff78f6ad6228c
steventruong1 <struong1@stevens.edu>
2022-12-09 18:57:46 -0500

    added mroe code stuff to work on

--- routes/comment.js
+++ routes/comment.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcrypt');
@@ -5,12 +6,22 @@ const helpFunctions = require("../helpers.js");
 const userData = require("../data/users.js");
 
 router
-  .route('/comment')
+  .route('/')
   .get(async (req, res) => {
     //code here for GET
     if(req.session.user)
     {
       //res.redirect('/protected');
+    }
+    else
+    {
+      res.render('userLogin', {title: "Login"});
+    }
+  })
+  .post(async (req, res) => {
+    if(req.session.user)
+    {
+      //res.render('userLogin', {title: "Login"}); Needs to add a new page
     }
     else
     {
