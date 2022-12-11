8d52f82e022ec44410aecf054505e91fd0dbb0d3
steventruong1 <struong1@stevens.edu>
2022-12-10 21:12:58 -0500

    Added Try Catches

--- routes/post.js
+++ routes/post.js
@@ -9,30 +9,37 @@ router
   .route('/')
   .get(async (req, res) => {
     //code here for GET
-    if(req.session.user)
-    {
-      //const postList = await postdata.getAllPosts();
-      //res.render('userLogin', {title: "Login"}); Need to render a page that shows a good amount of posts.
-    }
-    else
-    {
-      res.render('userLogin', {title: "Login"});
+    try {
+      if(req.session.user)
+      {
+        //const postList = await postdata.getAllPosts();
+        //res.render('userLogin', {title: "Login"}); Need to render a page that shows a good amount of posts.
+      }
+      else
+      {
+        res.render('userLogin', {title: "Login"});
+      }
+    } catch (error) {
+      
     }
   })
 
   router
   .route("/:postId")
   .get(async (req, res) => {
-    if (
-      !req.params.postId ||
-      helper.stringChecker(req.params.postId) ||
-      !ObjectId.isValid(req.params.postId)
-    ) {
-      // res.status(400).json({ error: "invalid id" });
-      // return null;
+    try {
+      helpFunctions.stringChecker(req.params.postId,"PostId")
+      if (
+        !ObjectId.isValid(req.params.postId)
+      ) {
+        // res.status(400).json({ error: "invalid id" });
+      }
+        //const post = await postdata.getPostByID(req.params.postId);
+        //res.render('userLogin', {title: "Login"}); Need to render a page that shows a good amount of posts.      
+    } catch (error) {
+      
     }
-      //const post = await postdata.getPostByID(req.params.postId);
-      //res.render('userLogin', {title: "Login"}); Need to render a page that shows a good amount of posts.
+
   })
 
 
@@ -41,61 +48,77 @@ router
   .route("/new")
   .get(async (req, res) => 
   {
-    if(req.session.user)
-    {
-      //res.render('userLogin', {title: "Login"}); Need to render a page that shows the post form
-    }
-    else
-    {
-      res.render('userLogin', {title: "Login"});
+    try {
+      if(req.session.user)
+      {
+        //res.render('userLogin', {title: "Login"}); Need to render a page that shows the post form
+      }
+      else
+      {
+        res.render('userLogin', {title: "Login"});
+      } 
+    } catch (error) {
+      
     }
   })
   .post(async (req, res) => {
-    if(req.session.user)
-    {
-      // buildingInput
-      // floorInput
-      // descInput
-      // noiseInput
-      // locationInput
-      // capacityInput
-      // nycInput
-      // foodInput
-      //const postList = await postdata.addPost();
-      //res.render('userLogin', {title: "Login"}); Render page with post
-    }
-    else
-    {
-      res.render('userLogin', {title: "Login"});
+    try {
+      if(req.session.user)
+      {
+        // buildingInput
+        // floorInput
+        // descInput
+        // noiseInput
+        // locationInput
+        // capacityInput
+        // nycInput
+        // foodInput
+        //const postList = await postdata.addPost();
+        //res.render('userLogin', {title: "Login"}); Render page with post
+      }
+      else
+      {
+        res.render('userLogin', {title: "Login"});
+      } 
+    } catch (error) {
+      
     }
   })
 
 router
   .route('/like')
   .post(async (req, res) => {
-    if(req.session.user)
-    {
-      //check that user has not liked the post before
-      //const postList = await postdata.addLike();
-      //res.render('userLogin', {title: "Login"}); Needs to refresh the page
-    }
-    else
-    {
-      res.render('userLogin', {title: "Login"});
+    try {
+      if(req.session.user)
+      {
+        //check that user has not liked the post before
+        //const postList = await postdata.addLike();
+        //res.render('userLogin', {title: "Login"}); Needs to refresh the page
+      }
+      else
+      {
+        res.render('userLogin', {title: "Login"});
+      } 
+    } catch (error) {
+      
     }
   })
 router
   .route('/dislike')
   .post(async (req, res) => {
-    if(req.session.user)
-    {
-      //check that user has not disliked the post before
-      //const postList = await postdata.removeLike ();
-      //res.render('userLogin', {title: "Login"}); Needs to refresh the page
-    }
-    else
-    {
-      res.render('userLogin', {title: "Login"});
+    try {
+      if(req.session.user)
+      {
+        //check that user has not disliked the post before
+        //const postList = await postdata.removeLike ();
+        //res.render('userLogin', {title: "Login"}); Needs to refresh the page
+      }
+      else
+      {
+        res.render('userLogin', {title: "Login"});
+      } 
+    } catch (error) {
+      
     }
   })
 module.exports = router ;
