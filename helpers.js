7f8a1adbd2e1dcb0247c6129e9d4b1a03ab8769b
Susan McAloon <smcaloon@stevens.edu>
2022-12-12 15:14:21 -0500

    added helper functions

--- helpers.js
+++ helpers.js
@@ -69,7 +69,7 @@ let checkStudentCapacity = (studentCapacity) => {
   const validCapacity = ['0','1','2','3','4','5', '6','7','8','9','10','11',
                        '12','13','14','15','16','17','18','19','20'];
   if (!validCapacity.includes(studentCapacity)) throw 'Student capacity must be integer from 1 to 20';
-  return validCapacity;
+  return studentCapacity;
 };
 
 
