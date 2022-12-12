f9dc5ed41c64778e90e097d5b41dbb3590d896b3
zatchiian <zachzajack@gmail.com>
2022-12-11 22:43:47 -0500

    not sure why these are here? just going to comment them out so I can continue testing...

--- routes/comment.js
+++ routes/comment.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const helpFunctions = require("../helpers.js");
-const commentData = require("../data/comments.js");
+//const commentData = require("../data/comments.js");
 const { ObjectId } = require("mongodb");
 
 router
