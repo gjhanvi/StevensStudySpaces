d1f880fd392f2026d9de80e555d08717764081ee
Susan McAloon <smcaloon@stevens.edu>
2022-12-14 15:09:09 -0500

    handle new post

--- routes/posts.js
+++ routes/posts.js
@@ -5,6 +5,74 @@ const helpFunctions = require("../helpers.js");
 const postdata = require("../data/posts.js");
 const { ObjectId } = require("mongodb");
 
+
+
+router
+.route('/new')
+.get(async (req, res) => 
+{
+  try {
+    if(req.session.user)
+    {
+      res.render('newPost', {title: "New Post"}); //Need to render a page that shows the post form
+    }
+    else
+    {
+      res.render('userLogin', {title: "Login"});
+    } 
+  } catch (error) {
+    console.log("error");
+  }
+})
+.post(async (req, res) => {
+  try {
+    if(req.session.user)
+    {
+      req.body.descInput = helpFunctions.stringChecker(req.body.descInput, 'Post Description');
+      req.body.noiseInput = helpFunctions.stringChecker(req.body.noiseInput, 'Noise rating');
+      req.body.noiseInput = helpFunctions.checkRating(req.body.noiseInput, 'noise');
+      req.body.locationInput = helpFunctions.stringChecker(req.body.locationInput, 'Location rating');
+      req.body.locationInput = helpFunctions.checkRating(req.body.locationInput, 'location');
+      req.body.nycInput = helpFunctions.stringChecker(req.body.nycInput, 'View rating');
+      req.body.nycInput = helpFunctions.checkRating(req.body.nycInput, 'View');
+      req.body.foodInput = helpFunctions.stringChecker(req.body.foodInput, 'Food input');
+      //req.body.foodInput = helpFunctions.checkFoodNear(req.body.foodInput);
+      req.body.capacityInput = helpFunctions.stringChecker(req.body.capacityInput, 'Capacity');
+      req.body.capacityInput = helpFunctions.checkStudentCapacity(req.body.capacityInput);
+      //still need to check photo
+      //still need to check req.session.userId
+      //still need to check building and floor
+      // buildingInput
+      // floorInput
+      // descInput
+      // noiseInput
+      // locationInput
+      // capacityInput
+      // nycInput
+      // foodInput
+      const post = await postdata.addPost(
+        req.session.userId,
+        req.body.buildingInput,
+        req.body.floorInput,
+        req.body.descInput,
+        req.body.noiseInput,
+        req.body.locationInput,
+        req.body.capacityInput,
+        req.body.nycInput,
+        req.body.photoInput,
+        req.body.foodInput
+        );
+        res.render('posts', {post: post, description: req.body.descInput}); //Render page with post
+    }
+    else
+    {
+      res.render('userLogin', {title: "Login"});
+    } 
+  } catch (error) {
+      res.render('newPost',{title: "New Post", error: error} );
+  }
+})
+
 router
   .route('/')
   .get(async (req, res) => {
@@ -43,71 +111,6 @@ router
 
   })
 
-  router
-  .route('/new')
-  .get(async (req, res) => 
-  {
-    try {
-      if(req.session.user)
-      {
-        res.render('newPost', {title: "New Post"}); //Need to render a page that shows the post form
-      }
-      else
-      {
-        res.render('userLogin', {title: "Login"});
-      } 
-    } catch (error) {
-      console.log("error");
-    }
-  })
-  .post(async (req, res) => {
-    try {
-      if(req.session.user)
-      {
-        req.body.descInput = helpFunctions.stringChecker(req.body.descInput, 'Post Description');
-        req.body.noiseInput = helpFunctions.stringChecker(req.body.noiseInput, 'Noise rating');
-        req.body.noiseInput = helpFunctions.checkRating(req.body.noiseInput, 'noise');
-        req.body.locationInput = helpFunctions.stringChecker(req.body.locationInput, 'Location rating');
-        req.body.locationInput = helpFunctions.checkRating(req.body.locationInput, 'location');
-        req.body.nycInput = helpFunctions.stringChecker(req.body.nycInput, 'View rating');
-        req.body.nycInput = helpFunctions.checkRating(req.body.nycInput, 'View');
-        req.body.foodInput = helpFunctions.stringChecker(req.body.foodInput, 'Food input');
-        //req.body.foodInput = helpFunctions.checkFoodNear(req.body.foodInput);
-        req.body.capacityInput = helpFunctions.stringChecker(req.body.capacityInput, 'Capacity');
-        req.body.capacityInput = helpFunctions.checkStudentCapacity(req.body.capacityInput);
-        //still need to check photo
-        //still need to check req.session.userId
-        //still need to check building and floor
-        // buildingInput
-        // floorInput
-        // descInput
-        // noiseInput
-        // locationInput
-        // capacityInput
-        // nycInput
-        // foodInput
-        const post = await postdata.addPost(
-          req.session.userId,
-          req.body.buildingInput,
-          req.body.floorInput,
-          req.body.descInput,
-          req.body.noiseInput,
-          req.body.locationInput,
-          req.body.capacityInput,
-          req.body.nycInput,
-          req.body.photoInput,
-          req.body.foodInput
-          );
-          res.render('posts', {post: post, description: req.body.descInput}); //Render page with post
-      }
-      else
-      {
-        res.render('userLogin', {title: "Login"});
-      } 
-    } catch (error) {
-        res.render('newPost',{title: "New Post", error: error} );
-    }
-  })
 
 router
   .route('/like')
